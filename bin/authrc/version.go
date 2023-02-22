186150ee72ccfc8729e8cc2c4dc78143ff83e750
TaMeR <gruessle@gmail.com>
2023-02-22 19:57:56 +0300

    version

--- bin/authrc/version.go
+++ bin/authrc/version.go
@@ -18,19 +18,19 @@ func init() {
 }
 
 //VerModTime is a UTC Unix time stamp
-const VerModTime = 1677081308
+const VerModTime = 1677084911
 
 //VerModDate is a UTC Date time stamp
-const VerModDate = "2023-02-22T15:55:08Z"
+const VerModDate = "2023-02-22T16:55:11Z"
 
 //VerLong is the full version from Git command output
-const VerLong = "v0.0.5-0-g2579f62"
+const VerLong = "v0.0.6-0-g7860c4d"
 
 //VerDirty means app was build with a git dir that contained modifications which had not been committed.
 const VerDirty = false
 
 //VerGit is the 7 hexadecimal digits version from Git.
-const VerGit = "g2579f62"
+const VerGit = "g7860c4d"
 
 //VerTag is the Tag version from Git.
-const VerTag = "v0.0.5"
+const VerTag = "v0.0.6"
